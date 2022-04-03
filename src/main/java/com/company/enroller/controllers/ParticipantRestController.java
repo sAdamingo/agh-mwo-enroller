aedc806ac62394029b9c62851180152e12ed60f1
Adam.Stelmach <adam.stelmach@fdmgroup.com>
2022-04-03 15:02:01 +0200

    Implemented POST and DELETE methods

--- src/main/java/com/company/enroller/controllers/ParticipantRestController.java
+++ src/main/java/com/company/enroller/controllers/ParticipantRestController.java
@@ -5,9 +5,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.company.enroller.model.Participant;
 import com.company.enroller.persistence.ParticipantService;
@@ -25,4 +23,23 @@ public class ParticipantRestController {
 		return new ResponseEntity<Collection<Participant>>(participants, HttpStatus.OK);
 	}
 
+	@RequestMapping(value = "/{login}", method = RequestMethod.GET)
+	public ResponseEntity<?> getParticipant(@PathVariable("login") String login) {
+		Participant participant = participantService.findByLogin(login);
+			if (participant == null) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			}
+			return new ResponseEntity<Participant>(participant,HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public ResponseEntity<?> registerParticipant(@RequestBody Participant participant) {
+		return participantService.addParticipant(participant);
+	}
+
+	@RequestMapping(value = "/{login}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> deleteParticipant(@PathVariable("login") String login) {
+		return participantService.deleteParticipant(login);
+
+	}
 }
--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,7 +2,10 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Participant;
@@ -22,4 +25,28 @@ public class ParticipantService {
 		return query.list();
 	}
 
+	public Participant findByLogin(String login) {
+		return connector.getSession().get(Participant.class, login);
+	}
+
+	public ResponseEntity addParticipant(Participant participant) {
+		if (findByLogin(participant.getLogin()) != null) {
+			return new ResponseEntity("Unable to create. A participant with login " + participant.getLogin() + " already exist.", HttpStatus.CONFLICT);
+		}
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
+		return new ResponseEntity(HttpStatus.OK);
+	}
+
+	public ResponseEntity deleteParticipant(String login) {
+		Participant participant = findByLogin(login);
+		if (participant == null) {
+			return new ResponseEntity("Unable to delete. Participant does not exist.", HttpStatus.CONFLICT);
+		}
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(participant);
+		transaction.commit();
+		return new ResponseEntity(HttpStatus.OK);
+	}
 }
